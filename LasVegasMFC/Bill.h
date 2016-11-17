888871ef518fd54f9393429faf5442c786edc257
황정태 <monawin456@gmail.com>
2016-11-17 17:31:35 +0900

    Add 'BillDeck' in 'Bill'.

--- LasVegasMFC/Bill.h
+++ LasVegasMFC/Bill.h
@@ -2,17 +2,17 @@
 class Bill
 {
 private:
-	int _1;
-	int _2;
-	int _3;
-	int _4;
-	int _5;
-	int _6;
-	int _7;
-	int _8;
-	int _9;
+	int price;
 public:
 	Bill();
 	~Bill();
+	int getPrice();
+};
+
+class BillDeck {
+private:
+public:
+	BillDeck();
+	~BillDeck();
 };
 
