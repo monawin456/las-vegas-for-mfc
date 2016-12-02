039c2be92c4153676316e9ba2cd511311aa02ccd
boydket <boydket@naver.com>
2016-12-02 20:28:34 +0900

    Update resource.

--- LasVegasMFC/Resource.h
+++ LasVegasMFC/Resource.h
@@ -2,12 +2,71 @@
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
 // LasVegasMFC.rc에서 사용되고 있습니다.
 //
+#define kc                              3
+#define ID_WHITE_DICE_Q                 3
 #define IDD_ABOUTBOX                    100
 #define IDP_OLE_INIT_FAILED             100
 #define IDD_LASVEGASMFC_FORM            101
 #define IDR_MAINFRAME                   128
 #define IDR_LasVegasMFCTYPE             130
 #define IDD_DICEDLG                     310
+#define ID_BLACK_DICE_1                 312
+#define ID_BLACK_DICE_2                 313
+#define ID_BLACK_DICE_3                 314
+#define ID_BLACK_DICE_4                 315
+#define ID_BLACK_DICE_5                 316
+#define ID_BLACK_DICE_6                 317
+#define ID_BLACK_DICE_Q                 318
+#define ID_BLUE_DICE_1                  319
+#define ID_BLUE_DICE_2                  320
+#define ID_BLUE_DICE_3                  321
+#define ID_BLUE_DICE_4                  322
+#define ID_BLUE_DICE_5                  323
+#define ID_BLUE_DICE_6                  324
+#define ID_BLUE_DICE_Q                  325
+#define ID_CASINO_1                     326
+#define ID_CASINO_2                     327
+#define ID_CASINO_3                     328
+#define ID_CASINO_4                     329
+#define ID_CASINO_5                     330
+#define ID_CASINO_6                     331
+#define ID_DOLLAR_1                     332
+#define ID_DOLLAR_2                     333
+#define ID_DOLLAR_3                     334
+#define ID_DOLLAR_4                     335
+#define ID_DOLLAR_5                     336
+#define ID_DOLLAR_6                     337
+#define ID_DOLLAR_7                     338
+#define ID_DOLLAR_8                     339
+#define ID_DOLLAR_9                     340
+#define ID_GREEN_DICE_1                 341
+#define ID_GREEN_DICE_2                 342
+#define ID_GREEN_DICE_3                 343
+#define ID_GREEN_DICE_4                 344
+#define ID_GREEN_DICE_5                 345
+#define ID_GREEN_DICE_6                 346
+#define ID_GREEN_DICE_Q                 347
+#define ID_RED_DICE_1                   348
+#define ID_RED_DICE_2                   349
+#define ID_RED_DICE_3                   350
+#define ID_RED_DICE_4                   351
+#define ID_RED_DICE_5                   352
+#define ID_RED_DICE_6                   353
+#define ID_RED_DICE_Q                   354
+#define ID_WHITE_DICE_1                 355
+#define IDB_PNG1                        356
+#define ID_WHITE_DICE_2                 357
+#define ID_WHITE_DICE_3                 358
+#define ID_WHITE_DICE_4                 359
+#define ID_WHITE_DICE_5                 360
+#define ID_WHITE_DICE_6                 361
+#define ID_YELLOW_DICE_1                362
+#define ID_YELLOW_DICE_2                363
+#define ID_YELLOW_DICE_3                364
+#define ID_YELLOW_DICE_4                365
+#define ID_YELLOW_DICE_5                366
+#define ID_YELLOW_DICE_6                367
+#define ID_YELLOW_DICE_Q                368
 #define IDC_STR                         1000
 #define IDC_BUTTON1                     1001
 #define IDC_BUTTON2                     1002
@@ -22,7 +81,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        312
+#define _APS_NEXT_RESOURCE_VALUE        371
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           310
